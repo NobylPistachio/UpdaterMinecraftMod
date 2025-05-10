8bfd0e51479f99c19df6ec1de975b431d2f5a9d0
NobylPistachio <NobylPistachio@gmail.com>
2025-05-10 17:02:06 -0400

    adding plan

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+#url = "https://www.curseforge.com/minecraft-bedrock/addons/serp-pokemon-addon"
+
+# Connect to the website
+# Download Files
+# Put files into folder
+# Open Aternos Server
+# Remove Olds Files 
+# Add New Files
+
+
+
